2f20fd465f1dea065af4875338dcb1d50148b0cb
edram <edram@qq.com>
2021-08-16 10:58:49 +0800

    初始化项目

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: '[name].[chunkhash].bundle.js',
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "",
+      template: "src/index.html",
+    }),
+  ],
+};

74dff14d3e727c8de12cc6cc3d976eee69c1ec68
edram <edram@qq.com>
2021-08-16 13:16:54 +0800

    chore: 增加 css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,4 +14,15 @@ module.exports = {
       template: "src/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
 };
